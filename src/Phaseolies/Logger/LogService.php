f840f5b219659040ad93a14c640673c66b0b558f
Arif Hoque <arif@sulaco-tec.com>
2025-09-08 18:16:12 +0600

    log class updated comment :

--- src/Phaseolies/Logger/LogService.php
+++ src/Phaseolies/Logger/LogService.php
@@ -9,12 +9,16 @@ use Monolog\Logger;
 class LogService implements ResettableInterface
 {
     /**
-     * @var Logger The Monolog logger instance.
+     * The Monolog logger instance.
+     *
+     * @var Logger
      */
     protected Logger $logger;
 
     /**
-     * @var LogHandlerInterface[] Array of log handlers.
+     * Array of log handlers.
+     *
+     * @var LogHandlerInterface[]
      */
     protected array $handlers = [];
 
@@ -31,8 +35,6 @@ class LogService implements ResettableInterface
 
     /**
      * Configures and returns a Monolog logger instance with specific handlers.
-     *
-     * This method sets up a logger with configured handlers.
      *
      * @param string|null $channel The logging channel.
      * @return Logger The configured Monolog logger instance.
@@ -52,9 +54,6 @@ class LogService implements ResettableInterface
 
     /**
      * Resets the logger state by clearing all handlers.
-     *
-     * This method is required by the ResettableInterface and allows for
-     * resetting the logger to its initial state.
      *
      * @return void
      */
