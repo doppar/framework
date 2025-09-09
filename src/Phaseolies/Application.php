ada4ea6aad0beb9ab9f0a4191437ce68e012eacc
Arif Hoque <arif@sulaco-tec.com>
2025-09-09 15:37:42 +0600

    up

--- src/Phaseolies/Application.php
+++ src/Phaseolies/Application.php
@@ -17,7 +17,7 @@ class Application extends Container
     /**
      * The current version of the Doppar framework.
      */
-    const VERSION = '2.1.9.8';
+    const VERSION = '2.9.2.0';
 
     /**
      * The base path of the application installation.
