50c72c7f706debb63f798a64a82d9fec09e9e111
techmahedy <31318802+techmahedy@users.noreply.github.com>
2025-11-15 11:33:00 +0000

    Update version to v3.0.0-beta.12

--- src/Phaseolies/Application.php
+++ src/Phaseolies/Application.php
@@ -17,7 +17,7 @@ class Application extends Container
     /**
      * The current version of the Doppar framework.
      */
-    const VERSION = '3.0.0-beta.11';
+    const VERSION = '3.0.0-beta.12';
 
     /**
      * The base path of the application installation.
