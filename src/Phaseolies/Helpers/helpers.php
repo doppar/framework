7b942d4a820d45a981dc4ef870ea8f67819d32df
Arif Hoque <arif@sulaco-tec.com>
2025-11-28 10:50:47 +0600

    headers already sent issue resolved for fallback error status code

--- src/Phaseolies/Helpers/helpers.php
+++ src/Phaseolies/Helpers/helpers.php
@@ -18,6 +18,7 @@ use Phaseolies\DI\Container;
 use Phaseolies\Config\Config;
 use Phaseolies\Cache\RateLimiter;
 use Phaseolies\Auth\Security\Authenticate;
+use Doppar\Airbend\Support\Facades\Broadcast;
 use Carbon\Carbon;
 
 if (!function_exists('env')) {
@@ -902,5 +903,19 @@ if (!function_exists('throttle')) {
     function throttle(): RateLimiter
     {
         return app(RateLimiter::class);
+    }
+}
+
+if (!function_exists('broadcast')) {
+    function broadcast(string|array $channels, $event): void
+    {
+        Broadcast::channel($channels, $event);
+    }
+}
+
+if (!function_exists('broadcast_to_others')) {
+    function broadcast_to_others(string|array $channels, $event): void
+    {
+        Broadcast::toOthers()->channel($channels, $event);
     }
 }
