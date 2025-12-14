e11e69e3159a805583c3f5a765e8dec9d6c400ac
rrr63 <46089601+rrr63@users.noreply.github.com>
2025-12-14 16:11:42 +0000

    Update version to v3.1.0

--- src/Phaseolies/Application.php
+++ src/Phaseolies/Application.php
@@ -17,7 +17,7 @@ class Application extends Container
     /**
      * The current version of the Doppar framework.
      */
-    const VERSION = '3.0.4';
+    const VERSION = '3.1.0';
 
     /**
      * The base path of the application installation.
