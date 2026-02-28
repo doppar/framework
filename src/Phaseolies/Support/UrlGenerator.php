22b11b54557b84e501d5e71ec99172c145135371
Arif Hoque <arif@sulaco-tec.com>
2026-02-28 22:04:23 +0600

    fix: base_url() implementation updated for domain tenant:

--- src/Phaseolies/Support/UrlGenerator.php
+++ src/Phaseolies/Support/UrlGenerator.php
@@ -56,7 +56,7 @@ class UrlGenerator
      */
     public function __construct(?string $baseUrl = null, ?bool $secure = null)
     {
-        $this->baseUrl = $baseUrl ? rtrim($baseUrl, '/') : $this->determineBaseUrl();
+        $this->baseUrl = $this->determineBaseUrl() ?? $baseUrl;
 
         $this->secure = $secure ?? $this->isSecureRequest();
     }
