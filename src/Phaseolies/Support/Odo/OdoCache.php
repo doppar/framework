7282c9865e6fd36b4eab86b764f3be72caab4181
Arif Hoque <arif@sulaco-tec.com>
2026-03-09 15:12:13 +0600

    improved file setter for windows mac ubuntu:

--- src/Phaseolies/Support/Odo/OdoCache.php
+++ src/Phaseolies/Support/Odo/OdoCache.php
@@ -27,12 +27,16 @@ trait OdoCache
      */
     public function createCacheFolder(): void
     {
-        $actual = base_path() . '/' . $this->cacheFolder;
+        $actual = base_path() . DIRECTORY_SEPARATOR . $this->cacheFolder;
 
         if (!is_dir($actual)) {
             if (!mkdir($actual, 0755, true) && !is_dir($actual)) {
                 throw new RuntimeException('Unable to create view cache folder: ' . $actual);
             }
+        }
+
+        if (!is_writable($actual)) {
+            throw new RuntimeException('View cache folder is not writable: ' . $actual);
         }
     }
 
@@ -43,12 +47,16 @@ trait OdoCache
      */
     public function createPublicSymlinkFolder(): void
     {
-        $actual = base_path() . '/' . $this->publicSymlinkFolder;
+        $actual = base_path() . DIRECTORY_SEPARATOR . $this->publicSymlinkFolder;
 
         if (!is_dir($actual)) {
             if (!mkdir($actual, 0755, true) && !is_dir($actual)) {
                 throw new RuntimeException('Unable to create app/public cache folder: ' . $actual);
             }
+        }
+
+        if (!is_writable($actual)) {
+            throw new RuntimeException('App/public folder is not writable: ' . $actual);
         }
     }
 
@@ -60,12 +68,13 @@ trait OdoCache
     public function clearCache(): bool
     {
         $extension = ltrim($this->fileExtension, '.');
-        $files = glob($this->cacheFolder . DIRECTORY_SEPARATOR . '*.' . $extension);
+
+        $files = glob(str_replace('\\', '/', $this->cacheFolder) . '/*.' . $extension);
         $result = true;
 
         foreach ($files as $file) {
-            if (is_file($file)) {
-                $result = @unlink($file);
+            if (is_file($file) && !@unlink($file)) {
+                $result = false;
             }
         }
 
@@ -79,7 +88,7 @@ trait OdoCache
      */
     public function setCacheFolder($path): void
     {
-        $this->cacheFolder = str_replace('/', DIRECTORY_SEPARATOR, $path);
+        $this->cacheFolder = rtrim(str_replace('/', DIRECTORY_SEPARATOR, $path), DIRECTORY_SEPARATOR);
     }
 
     /**
@@ -89,6 +98,6 @@ trait OdoCache
      */
     public function setSymlinkPathFolder($path): void
     {
-        $this->publicSymlinkFolder = str_replace('/', DIRECTORY_SEPARATOR, $path);
+        $this->publicSymlinkFolder = rtrim(str_replace('/', DIRECTORY_SEPARATOR, $path), DIRECTORY_SEPARATOR);
     }
 }
