32f55d058fe1fb690a1dfa3c3cca26c95ff5bb27
Arif Hoque <arif@sulaco-tec.com>
2026-05-06 15:35:03 +0600

    Fix: Make Response body contract explicitly nullable

--- src/Phaseolies/Http/Response.php
+++ src/Phaseolies/Http/Response.php
@@ -122,9 +122,9 @@ class Response implements HttpStatus
     /**
      * Get the response body content.
      *
-     * @return string
+     * @return string|null
      */
-    public function getBody(): string
+    public function getBody(): ?string
     {
         return $this->body;
     }
